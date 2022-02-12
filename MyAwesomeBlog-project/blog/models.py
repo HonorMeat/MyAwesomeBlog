f6325aac8b9f5b1306d7ba00bf9e6d67f9a393ab
HonorMeat <fig010104@mail.ru>
2022-02-13 00:40:30 +0600

    Add Posts model

--- MyAwesomeBlog-project/blog/models.py
+++ MyAwesomeBlog-project/blog/models.py
@@ -1,3 +1,9 @@
+from cgitb import text
+from unicodedata import name
 from django.db import models
-
-# Create your models here.
+from django.forms import CharField
+class Posts(models.Model):
+    title = models.CharField(max_length=50, name='Заголовок')
+    date = models.DateTimeField(name='Дата')
+    text = models.TextField(name='Текст')
+    image = models.ImageField(upload_to='event_images/', name='Картинка')
